94c17f3437f9c1881be1fd82528df372aa7be23a
Anny Wang <astrjc.anny@gmail.com>
2022-03-30 09:40:40 -0700

    add the review component inside

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Reviews from './Reviews.js';
+
 
 const App = () => {
   return (
     <div>
       This is the App component.
+      <Reviews />
     </div>
   )
 };
