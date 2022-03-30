a1864fdbe8fcae34ef41b3296a985a77b27edf16
amyKwak <yoomikwak@gmail.com>
2022-03-30 02:50:53 -0700

    add to cart complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+
 import styled from 'styled-components';
 import { StarIcon } from '@heroicons/react/solid'
+import Navbar from './Navbar.js';
 
 
 const Button = styled.button`
@@ -11,7 +13,11 @@ const Button = styled.button`
   margin: 0 1em;
   padding: 0.25em 1em;
 `
-
+const ProductName = styled.h1`
+font-family: 'Poppins', sans-serif;
+font-weight: 500;
+font-size: 20px;
+`
 
 
 
@@ -20,26 +26,33 @@ function App() {
   const [cart, setCart] = useState({items: 0, products: [{}]})
   const [product, setProduct] = useState([])
   const [style, setStyle] = useState({});
-  const [price, setPrice] = useState(40)
+  const [price, setPrice] = useState(40);
+
+  function decrementCount() {
+    cart.items > 0 ? setCart(prevCart => ({items: prevCart.items -1})) : null
+  }
 
   return (
     <div>
-      <h2>Morning Joggers</h2>
-      <StarIcon className="star"></StarIcon>
-      <div className="price">$ {price}</div>
+      <Navbar>Threads</Navbar>
+      <div className="container">
+        <ProductName>Morning Joggers</ProductName>
+        <StarIcon className="star"></StarIcon>
+        <div className="price">$ {price}</div>
 
-      <div>Whether you're a morning person or not. Whether you're gym bound or not. Everyone looks good in joggers.</div>
-      <p className="cart">Cart: {cart.items}</p>
-      <Button onClick={() => setCart({items: cart.items + 1})}>Add to Cart</Button>
-      Quantity:
-      <button>+</button>
-      0
-      <button>-</button>
-      <select>
-        <option>Small</option>
-        <option>Medium</option>
-        <option>Large</option>
-      </select>
+        <div>Whether you're a morning person or not. Whether you're gym bound or not. Everyone looks good in joggers.</div>
+        <p className="cart">Cart: {cart.items}<button onClick={decrementCount}>-</button></p>
+        <Button onClick={() => setCart({items: cart.items + 1})}>Add to Cart</Button>
+        Quantity
+        <button>+</button>
+        0
+        <button>-</button>
+        <select>
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+        </select>
+      </div>
     </div>
   );
 }
