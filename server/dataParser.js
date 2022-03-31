a42ff6543b416ac62c91662dd25288c4ebb38177
Anny Wang <astrjc.anny@gmail.com>
2022-03-30 22:34:18 -0700

    adding all router

--- server/dataParser.js
+++ server/dataParser.js
@@ -15,21 +15,21 @@ fetchProductsData = (productID, styled, related) => {
   return new Promise ((resolve, reject)=> {
     if (related) {
       axios.get(`${url}/products/${productID}/related`, config)
-      .then(res => resolve(res.data))
-      .catch(err => reject(err))
+        .then(res => resolve(res.data))
+        .catch(err => reject(err))
     } else if (styled) {
       axios.get(`${url}/products/${productID}/styles`, config)
-      .then(res => resolve(res.data))
-      .catch(err => reject(err))
+        .then(res => resolve(res.data))
+        .catch(err => reject(err))
     }
     else if (productID) {
       axios.get(`${url}/products/${productID}`, config)
-      .then(res => resolve(res.data))
-      .catch(err => reject(err))
+        .then(res => resolve(res.data))
+        .catch(err => reject(err))
     } else {
       axios.get(`${url}/products`, config)
-      .then(res => resolve(res.data))
-      .catch(err => reject(err))
+        .then(res => resolve(res.data))
+        .catch(err => reject(err))
     }
   })
 }
@@ -37,39 +37,137 @@ fetchProductsData = (productID, styled, related) => {
 fetchReviewsData = (productID = '65632') => {
   return new Promise ((resolve, reject) => {
     axios.get(`${url}/reviews?product_id=${productID}`, config)
-    .then(res => resolve(res.data))
-    .catch(err => reject(err))
+      .then(res => resolve(res.data))
+      .catch(err => reject(err))
   })
+}
+
+fetchReviewMeta = (productID = '65632') => {
+  return new Promise((resolve, reject) => {
+    axios.get(`${url}/rereviews/meta/product_id=${productID}`, config)
+      .then(res => resolve(res.data))
+      .catch(err => reject(err))
+  })
+}
+
+addReview = (data) => {
+  return new Promise((resolve, reject) => {
+    axios.post(`${url}/reviews`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
+  })
+}
+
+markReviewAsHelpful = (reviewID = '1136190') => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/reviews/${reviewID}/helpful`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
+  })
+}
+
+reportReview = (reviewID = '1136190') => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/reviews/${reviewID}/report`)
+  }, config)
+    .then(res => resolve(res))
+    .catch(err => reject(err))
 }
 
 fetchQuestionsData = (productID = '65632') => {
   return new Promise((resolve, reject) => {
     axios.get(`${url}/qa/questions?product_id=${productID}`, config)
-    .then(res => resolve(res.data))
-    .catch(err=> reject(err))
+      .then(res => resolve(res.data))
+      .catch(err=> reject(err))
   })
 }
 
 fetchAnswerData = (questionID = '573876') => {
   return new Promise((resolve, reject) => {
     axios.get(`${url}/qa/questions/${questionID}/answers`, config)
-    .then(res => resolve(res.data))
-    .catch(err=> reject(err))
+      .then(res => resolve(res.data))
+      .catch(err=> reject(err))
+  })
+}
+
+addQuestion = (data) => {
+  return new Promise((resolve, reject) => {
+    axios.post(`${url}/qa/questions`, data, config)
+      .then(res => resolve(res.data))
+      .catch(err => reject(err))
+  })
+}
+
+addAnswer = (questionID, data) => {
+  return new Promise((resolve, reject) => {
+    axios.post(`${url}/qa/questions/${questionID}/answers
+    `, data, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
+  })
+}
+
+makeQuestionAsHelpful = (questionID) => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/qa/questions/${questionID}/helpful
+  `, config)
+    .then(res => resolve(res))
+    .catch(err => reject(err))
+  })
+}
+
+reportQuestion = (questionID) => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/qa/questions/${questionID}/report`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
+  })
+}
+makeAnswerAsHelpful = (answerID) => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/qa/answers/${answerID}/helpful`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
+  })
+}
+
+reportAnswer = (answerID) => {
+  return new Promise((resolve, reject) => {
+    axios.put(`${url}/qa/answers/${answerID}/report`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
   })
 }
 
 fetchCartData = () => {
   return new Promise((resolve, reject )=> {
     axios.get(`${url}/cart`, config)
-    .then(res => resolve(res.data))
-    .catch(err => reject(err))
+      .then(res => resolve(res.data))
+      .catch(err => reject(err))
+  })
+}
+addCart = (id) => {
+  return new Promise((resolve, reject) => {
+    axios.post(`${url}/cart`, config)
+      .then(res => resolve(res))
+      .catch(err => reject(err))
   })
 }
 
 module.exports = {
   fetchProductsData,
   fetchReviewsData,
+  fetchReviewMeta,
+  markReviewAsHelpful,
+  reportReview,
   fetchQuestionsData,
   fetchAnswerData,
+  addQuestion,
+  addAnswer,
+  makeQuestionAsHelpful,
+  reportQuestion,
+  makeAnswerAsHelpful,
+  reportAnswer,
   fetchCartData,
+  addCart
 }
--- server/index.js
+++ server/index.js
@@ -26,9 +26,7 @@ app.get('/products/:product_id', (req, res) => {
 
 app.get('/products/:product_id/styles', (req, res) => {
   let productID = req.params.product_id;
-  console.log(productID)
   let style = true;
-  console.log(style)
   fetchData.fetchProductsData(productID, style)
     .then(result => res.send(result))
 })
@@ -48,7 +46,28 @@ app.get('/reviews', (req, res) => {
   let productID = req.query.product_id;
   fetchData.fetchReviewsData(productID)
     .then(result => res.send(result))
+})
 
+app.get('/reviews/meta', (req, res) => {
+  let productID = req.query.product_id;
+  fetchData.fetchReviewMeta(productID)
+    .then(result => res.send(result))
+})
+
+app.post('/reviews', (req, res) => {
+  fetchData.addReview(req.body)
+    .then(result => res.send(201))
+})
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  console.log(req.params.review_id)
+  fetchData.markReviewAsHelpful(req.params.review_id)
+    .then(result => res.send(204))
+})
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  fetchData.reportReview(req.params.review_id)
+    .then(result => res.send(204))
 })
 
 // Questions and Answers
@@ -59,10 +78,40 @@ app.get('/qa/questions', (req, res) => {
 })
 
 app.get('/qa/questions/:question_id/answers', (req, res) => {
-  let questionID = req.query.question_id;
+  let questionID = req.params.question_id;
   let answers = true;
   fetchData.fetchAnswerData(questionID)
     .then(result => res.send(result))
+})
+
+app.post('/qa/questions', (req, res) => {
+  fetchData.addQuestion(req.body)
+    .then(result => res.send(201))
+})
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  fetchData.appAnswer(req.params.question_id, req.body)
+    .then(result => res.send(201))
+})
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  fetchData.makeQuestionAsHelpful(req.params.question_id)
+    .then(result => res.send(204))
+})
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  fetchData.reportQuestion(req.params.question_id)
+    .then(result => res.send(204))
+})
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  fetchData.makeAnswerAsHelpful(req.params.answer_id)
+    .then(result => res.send(204))
+})
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  fetchData.reportAnswer(req.parmas.answer_id)
+    .then(result => res.send(204))
 })
 
 // Cart
@@ -70,6 +119,11 @@ app.get('/cart', (req, res) => {
   fetchData.fetchCartData()
     .then(result => res.send(result))
 })
+app.post('/cart', (req, res) => {
+  fetchData.addCart(req.body)
+    .then(result => res.send(201))
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
