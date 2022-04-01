c6b0ae5c8be169eda99d38b8be951df561cb8068
Anny Wang <astrjc.anny@gmail.com>
2022-04-01 03:49:21 -0700

    add the rate display

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -9,7 +9,14 @@ import ReviewSearch from './ReviewSearch.js'
 
 const RatingAndReview = styled.section`
 padding: 4em;
+display: flex;
+justify-content: flex-start;
 `;
+
+const RatingDiv = styled.div`
+padding-right: 50px
+`
+
 const Button = styled.button`
 background-color: #112D4E;
 color: #F9F7F7
@@ -21,10 +28,17 @@ flex-direction: row;
 flex-wrap: nowrap;
 justify-content: flex-start;
 `
+const Response = styled.div`
+background-color: transparent
+`
 
 const ReviewDiv = styled.div`
 height: 400px;
+width: 800px;
 overflow: auto;
+`
+const ratingUserContainer = styled.div`
+display: inline-block;
 `
 
 function fetchData(setData, count) {
@@ -50,27 +64,44 @@ const Reviews = () => {
   let writable = (!isWritable) ? 'hidden' : ''
   let reviews = data.map(review => (
     <div className='review' key={review.id}>
-     <h3>rating: {review.rating}</h3>
-      <h2 className='title'>Summary: {review.summary}</h2>
+      <div><h3>rating: {review.rating}</h3>by {review.reviewer_name}</div>
+      <h2 className='title'>{review.summary}</h2>
       <h3>recommend: {review.recommend.toString()}</h3>
       <Photos>
       {review.photos.map(photo => (
-        <div id={photo.id}><img src={photo.url} width='150' /></div>
+      <div id={photo.id}><img src={photo.url} width='150' /></div>
       ))}
       </Photos>
-      <h4>Reviewer: {review.reviewer_name}</h4>
 
       {/* <h3>{moment().format({review.date}).fromNow()}</h3> */}
-      <h3>{review.body}</h3>
-      <h3>Response: {review.response}</h3>
+      <a>{review.body}</a>
+      <Response>Response: {review.response}</Response>
+
+      <a>Helpful? Yes(5) | Report</a>
+      <hr />
     </div>
   ))
   return (
+    <div>Ratings and Reviews
+
     <RatingAndReview>
-        <div><h2>Reviews</h2></div>
-        <div>Rank</div><StarIcon className='star'/>
+        <RatingDiv>Rate
+          <h1>4.0</h1>
+          <h4>100% of reviews recommend this product</h4>
+          <StarIcon className='star'/>
+          <StarIcon className='star'/>
+          <StarIcon className='star'/>
+          <StarIcon className='star'/>
+          <StarIcon className='star'/>
+          <h3>5 stars</h3>
+          <h3>4 stars</h3>
+          <h3>3 stars</h3>
+          <h3>2 stars</h3>
+          <h3>1 stars</h3>
+        </RatingDiv>
+
+        <div>
         <ReviewSearch />
-        <Button onClick={() => setisWritable(true)}>Write a Review</Button>
          <div className={writable}>
          <ReviewForm setisWritable={setisWritable}/>
          </div>
@@ -78,8 +109,10 @@ const Reviews = () => {
           {reviews}
         </ReviewDiv>
         <Button onClick={() => {fetchMoreData(setData, setCount, count)} }>More Reviews</Button>
-
+        <Button onClick={() => setisWritable(true)}>Write a Review</Button>
+        </div>
     </RatingAndReview>
+    </div>
   )
 }
 
