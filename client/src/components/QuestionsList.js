a8f2f924ccd40a09f59262d29f504d7decdb1b76
Jake Gilfix <38677018+jakemgilfix@users.noreply.github.com>
2022-04-04 15:47:40 -0700

    Fixes syntax issue (#17)
    Co-authored-by: Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>

--- client/src/components/QuestionsList.js
+++ client/src/components/QuestionsList.js
@@ -17,9 +17,9 @@ const QATextColumn = styled.col`
   width: 100%;
 `;
 
-const MoreQuestions = styled.td`
-  text-align: center;
-`;
+// const MoreQuestions = styled.td`
+//   text-align: center;
+// `;
 
 const QuestionsList = () => {
   const productID = 65633; // this will obviously need to be passed as a prop/through context
@@ -31,31 +31,47 @@ const QuestionsList = () => {
 
   useEffect(() => {
     axios.get('/qa/questions', { params: { product_id: productID } })
-      .then(res => setQuestions(res.data.results.sort(helpfulnessComparator)))
-      .catch(err => console.error(`Error getting questions & answers: ${err}`));
+      .then((res) => setQuestions(res.data.results.sort(helpfulnessComparator)))
+      .catch((err) => console.error(`Error getting questions & answers: ${err}`));
   }, []);
 
   return (
-    <table>
-      <QAColumn span="1" />
-      <QATextColumn span="1" />
+    <QuestionsTable>
+      <colgroup>
+        <QAColumn span="1" />
+        <QATextColumn span="1" />
+      </colgroup>
       <tbody>
         {questions?.map((q, i) => (
           (i >= displayLimit)
-          ? null
-          : <QuestionsListEntry question={q} key={i} />
+            ? null
+            : <QuestionsListEntry question={q} key={i} />
         ))}
         <tr>
-          <td colspan="2" style={{textAlign: "center"}}>
+          <td colSpan="2" style={{ textAlign: 'center' }}>
             {questions.length > displayLimit
-              ? <button onClick={() => setDisplayLimit(Number.POSITIVE_INFINITY)}>MORE ANSWERED QUESTIONS</button>
-              : questions.length && <button onClick={() => setDisplayLimit(4)}>COLLAPSE QUESTIONS</button> || null
-            }
+              ? (
+                <button
+                  type="button"
+                  onClick={() => setDisplayLimit(Number.POSITIVE_INFINITY)}
+                >
+                  MORE ANSWERED QUESTIONS
+                </button>
+              )
+              : (questions.length
+              && (
+              <button
+                type="button"
+                onClick={() => setDisplayLimit(4)}
+              >
+                COLLAPSE QUESTIONS
+              </button>
+              )) || null}
           </td>
         </tr>
       </tbody>
-    </table>
-  )
-}
+    </QuestionsTable>
+  );
+};
 
 export default QuestionsList;
