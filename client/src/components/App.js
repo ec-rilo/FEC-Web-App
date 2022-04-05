803443a0c917f7b29ddcbcc14976c53b786fccb4
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-05 14:24:18 -0700

    Updated path for Questions component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Overview from './Overview/Overview.js';
 import Reviews from './Reviews.js';
-import Questions from './Questions.js';
+import Questions from './QandA/Questions';
 import RelatedItems from './RelatedItems.js';
 import Navbar from './Navbar/Navbar.js';
 
