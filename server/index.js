fab7f8b5e5dc86e4b74ddb17738b7e57b418ab3b
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-13 22:14:31 -0700

    feat: adds fallback routes to allow for front-end routing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +11,12 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.static('client/dist')); // serve up the static files like index.html
 app.use(express.json());
 app.use('', router);
+
+// Fall back to routes on front-end from React Router
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
