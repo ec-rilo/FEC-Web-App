a52d9691d2626e5ed851ccd3881d0ad05d0bf347
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-14 14:50:51 -0700

    sends compressed bundle if it can be accepted

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 
@@ -8,7 +9,8 @@ const router = require('./routes');
 
 const PORT = 3000 || process.env.PORT;
 
-app.use(express.static('client/dist')); // serve up the static files like index.html
+// app.use(express.static('client/dist')); // serve up the static files like index.html
+app.use('/', expressStaticGzip('client/dist', { enableBrotli: true }));
 app.use(express.json());
 app.use('', router);
 
