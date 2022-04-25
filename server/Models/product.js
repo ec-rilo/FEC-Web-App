04c63218d10c1645010ea11ec28f47b7c5509a60
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-25 10:20:21 -0700

    feat: add getStyles function

--- server/Models/product.js
+++ server/Models/product.js
@@ -10,6 +10,80 @@ pool.connect()
   .then(() => console.log('connected to postgres!'))
   .catch((err) => console.log('failed to connect: ', err));
 
+const createUnionQueryStr = (arr, tableName) => {
+  let combinedQuery = '';
+
+  arr.forEach((style, index) => {
+    const photosQuery = `(SELECT * FROM ${tableName} WHERE ${tableName}.style_id = $${index + 1})`;
+    if (index !== arr.length - 1) {
+      combinedQuery = `${combinedQuery + photosQuery}\nUNION ALL\n`;
+    } else {
+      combinedQuery = `${combinedQuery + photosQuery};`;
+    }
+  });
+
+  return combinedQuery;
+};
+
+const filterIntoArr = (oldArr, oldDataArr) => {
+  const arr = [];
+
+  oldArr.forEach((style) => {
+    let sliceLength = 0;
+
+    for (let i = 0; i < oldDataArr.length; i++) {
+      if (oldDataArr[i].style_id === style.id) {
+        sliceLength = i + 1;
+      } else {
+        break;
+      }
+    }
+
+    const newArr = oldDataArr.splice(0, sliceLength);
+    arr.push(newArr);
+  });
+
+  return arr;
+};
+
+const getIdArr = (arr) => {
+  const idArr = [];
+
+  arr.forEach((style) => {
+    idArr.push(style.id);
+  });
+
+  return idArr;
+};
+
+function SkuNode(obj) {
+  this.style_id = obj.style_id;
+  this.quantity = obj.quantity;
+  this.size = obj.size;
+}
+
+const makeSkusObj = (arr) => {
+  const skus = {};
+
+  arr.forEach((obj) => {
+    const SkuObj = new SkuNode(obj);
+    skus[obj.id] = SkuObj;
+  });
+
+  return skus;
+};
+
+const convertToSkuObjs = (arr) => {
+  let newArr = arr.slice();
+
+  newArr = newArr.map((subArr) => {
+    const skusObj = makeSkusObj(subArr);
+    return skusObj;
+  });
+
+  return newArr;
+};
+
 module.exports = {
   getSingleProduct: (id) => new Promise((resolve, reject) => {
     const productsQuery = 'SELECT * FROM Products WHERE Products.id = $1';
@@ -26,6 +100,43 @@ module.exports = {
         newData.features = features.rows;
 
         resolve(newData);
+      })
+      .catch((err) => reject(err));
+  }),
+  getAllStyles: (productId) => new Promise((resolve, reject) => {
+    const stylesQuery = 'SELECT * FROM Styles WHERE Styles.product_id = $1';
+    const stylesObj = {};
+
+    pool.query(stylesQuery, [productId])
+      .then((styles) => {
+        stylesObj.results = styles.rows;
+
+        const photosQuery = createUnionQueryStr(stylesObj.results, 'Photos');
+        const idArr = getIdArr(stylesObj.results);
+        return pool.query(photosQuery, idArr);
+      })
+      .then((data) => {
+        const photosArr = data.rows;
+        const arrOfPhotos = filterIntoArr(stylesObj.results, photosArr);
+
+        arrOfPhotos.forEach((arr, index) => {
+          stylesObj.results[index].photos = arr;
+        });
+
+        const skusQuery = createUnionQueryStr(stylesObj.results, 'Skus');
+        const idArr = getIdArr(stylesObj.results);
+        return pool.query(skusQuery, idArr);
+      })
+      .then((data) => {
+        const skusArr = data.rows;
+        let arrOfSkus = filterIntoArr(stylesObj.results, skusArr);
+        arrOfSkus = convertToSkuObjs(arrOfSkus);
+
+        arrOfSkus.forEach((skusObj, index) => {
+          stylesObj.results[index].skus = skusObj;
+        });
+
+        resolve(stylesObj);
       })
       .catch((err) => reject(err));
   }),
--- server/dbroutes.js
+++ server/dbroutes.js
@@ -1,4 +1,3 @@
-const { Pool } = require('pg');
 const dbRouter = require('express').Router();
 const controllers = require('./Controllers/controllers');
 
@@ -7,86 +6,6 @@ dbRouter.get('/product', (req, res) => controllers.product.getProduct(req, res))
 
 /* --------------------------- Product Styles ---------------------------*/
 
-const pool = new Pool({
-  user: 'ecarrillo046',
-  password: '12345',
-  database: 'products',
-});
-
-// HELPERS
-const createPhotosQueryStr = (obj) => {
-  let combinedQuery = '';
-
-  obj.results.forEach((style, index) => {
-    const photosQuery = `(SELECT * FROM Photos WHERE Photos.style_id = $${index + 1})`;
-    if (index !== obj.results.length - 1) {
-      combinedQuery = `${combinedQuery + photosQuery}\nUNION ALL\n`;
-    } else {
-      combinedQuery = `${combinedQuery + photosQuery};`;
-    }
-  });
-
-  return combinedQuery;
-};
-
-const filterIntoArr = (oldArr, oldDataArr) => {
-  const arr = [];
-
-  oldArr.forEach((style) => {
-    let sliceLength = 0;
-
-    for (let i = 0; i < oldDataArr.length; i++) {
-      if (oldDataArr[i].style_id === style.id) {
-        sliceLength = i + 1;
-      } else {
-        break;
-      }
-    }
-
-    const newArr = oldDataArr.splice(0, sliceLength);
-    arr.push(newArr);
-  });
-
-  return arr;
-};
-
-const getAllStyles = (productId) => new Promise((resolve, reject) => {
-  const stylesQuery = 'SELECT * FROM Styles WHERE Styles.product_id = $1';
-  const stylesObj = {};
-
-  pool.query(stylesQuery, [productId])
-    .then((styles) => {
-      const idArr = [];
-      stylesObj.results = styles.rows;
-
-      const photosQuery = createPhotosQueryStr(stylesObj);
-
-      stylesObj.results.forEach((style) => {
-        idArr.push(style.id);
-      });
-
-      return pool.query(photosQuery, idArr);
-    })
-    .then((data) => {
-      const photosArr = data.rows;
-      const arrOfPhotos = filterIntoArr(stylesObj.results, photosArr);
-
-      arrOfPhotos.forEach((arr, index) => {
-        stylesObj.results[index].photos = arr;
-      });
-
-      resolve(stylesObj);
-    })
-    .catch((err) => reject(err));
-});
-
-const getStyles = (req, res) => {
-  const id = req.params.product_id;
-  getAllStyles(id)
-    .then((data) => res.send(data))
-    .catch((err) => res.send(err));
-};
-
-dbRouter.get('/product/:product_id/styles', (req, res) => getStyles(req, res));
+dbRouter.get('/product/:product_id/styles', (req, res) => controllers.product.getStyles(req, res));
 
 module.exports = dbRouter;
