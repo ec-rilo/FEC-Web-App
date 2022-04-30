cdb21b0119f50eac72ba368efe65064f1609e08e
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-29 19:48:02 -0700

    feat: refactor getAllStyles query

--- server/Models/product.js
+++ server/Models/product.js
@@ -12,80 +12,6 @@ pool.connect()
   .then(() => console.log('connected to postgres!'))
   .catch((err) => console.log('failed to connect: ', err));
 
-const createUnionQueryStr = (arr, tableName) => {
-  let combinedQuery = '';
-
-  arr.forEach((style, index) => {
-    const photosQuery = `(SELECT * FROM ${tableName} WHERE ${tableName}.style_id = $${index + 1})`;
-    if (index !== arr.length - 1) {
-      combinedQuery = `${combinedQuery + photosQuery}\nUNION ALL\n`;
-    } else {
-      combinedQuery = `${combinedQuery + photosQuery};`;
-    }
-  });
-
-  return combinedQuery;
-};
-
-const filterIntoArr = (oldArr, oldDataArr) => {
-  const arr = [];
-
-  oldArr.forEach((style) => {
-    let sliceLength = 0;
-
-    for (let i = 0; i < oldDataArr.length; i++) {
-      if (oldDataArr[i].style_id === style.style_id) {
-        sliceLength = i + 1;
-      } else {
-        break;
-      }
-    }
-
-    const newArr = oldDataArr.splice(0, sliceLength);
-    arr.push(newArr);
-  });
-
-  return arr;
-};
-
-const getIdArr = (arr) => {
-  const idArr = [];
-
-  arr.forEach((style) => {
-    idArr.push(style.style_id);
-  });
-
-  return idArr;
-};
-
-function SkuNode(obj) {
-  this.style_id = obj.style_id;
-  this.quantity = obj.quantity;
-  this.size = obj.size;
-}
-
-const makeSkusObj = (arr) => {
-  const skus = {};
-
-  arr.forEach((obj) => {
-    const SkuObj = new SkuNode(obj);
-    skus[obj.id] = SkuObj;
-  });
-
-  return skus;
-};
-
-const convertToSkuObjs = (arr) => {
-  let newArr = arr.slice();
-
-  newArr = newArr.map((subArr) => {
-    const skusObj = makeSkusObj(subArr);
-    return skusObj;
-  });
-
-  return newArr;
-};
-
 module.exports = {
   getSingleProduct: (id) => new Promise((resolve, reject) => {
     const query = `
@@ -108,39 +34,38 @@ module.exports = {
       .catch((err) => reject(err));
   }),
   getAllStyles: (productId) => new Promise((resolve, reject) => {
-    const stylesQuery = 'SELECT * FROM Styles WHERE Styles.product_id = $1';
-    const stylesObj = {};
+    const query = `
+    SELECT json_build_object(
+      'product_id', style.product_id,
+      'results', (SELECT
+        json_agg(
+          json_build_object(
+            'style_id', Styles.style_id,
+            'product_id', Styles.product_id,
+            'name', Styles.name,
+            'original_price', Styles.original_price,
+            'default?', Styles.default_style,
+            'photos', (SELECT
+              json_agg(
+                json_build_object(
+                  'url', Photos.url,
+                  'thumbnail_url', Photos.thumbnail_url
+                )
+              )
+              FROM Photos WHERE Styles.product_id = $1 AND Photos.style_id = Styles.style_id
+            )
+          )
+        )
+        FROM Styles WHERE Styles.product_id = $2
+      )
+    )
+    FROM Styles AS style
+      WHERE style.product_id = $3 LIMIT 1
+    `;
 
-    pool.query(stylesQuery, [productId])
-      .then((styles) => {
-        stylesObj.results = styles.rows;
-
-        const photosQuery = createUnionQueryStr(stylesObj.results, 'Photos');
-        const idArr = getIdArr(stylesObj.results);
-        return pool.query(photosQuery, idArr);
-      })
+    pool.query(query, [productId, productId, productId])
       .then((data) => {
-        const photosArr = data.rows;
-        const arrOfPhotos = filterIntoArr(stylesObj.results, photosArr);
-
-        arrOfPhotos.forEach((arr, index) => {
-          stylesObj.results[index].photos = arr;
-        });
-
-        const skusQuery = createUnionQueryStr(stylesObj.results, 'Skus');
-        const idArr = getIdArr(stylesObj.results);
-        return pool.query(skusQuery, idArr);
-      })
-      .then((data) => {
-        const skusArr = data.rows;
-        let arrOfSkus = filterIntoArr(stylesObj.results, skusArr);
-        arrOfSkus = convertToSkuObjs(arrOfSkus);
-
-        arrOfSkus.forEach((skusObj, index) => {
-          stylesObj.results[index].skus = skusObj;
-        });
-
-        resolve(stylesObj);
+        resolve(data.rows[0].json_build_object);
       })
       .catch((err) => reject(err));
   }),
